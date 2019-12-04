2f7cc30b600eb043b2e42e634d833410bf0f2329
Ananya Paul <manification10@users.noreply.github.com>
2019-12-04 16:52:48 -0500

    Update sound_play_from_computer.bash

--- sound_play_from_computer.bash
+++ sound_play_from_computer.bash
@@ -1,8 +1,13 @@
-tail -fn0 aud.txt | \
+tail -fn0 /dev/cu.SLAB_USBtoUART | \
 while read line ; do
-        echo "$line" | grep "mpg123"
-        if [ $? = 0 ]
-        then
-                mpg123 ghungroo-audio.mp3
+        echo $line
+        if echo $line | grep -q "1"; then
+		mpg123 Ghungroo_1.mp3
+        fi
+        if echo $line | grep -q "2"; then
+        	mpg123 Ghungroo_2.mp3
+        fi
+        if echo $line | grep -q "3"; then
+                mpg123 Ghungroo_3.mp3
         fi
 done
