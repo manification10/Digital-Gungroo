7027fb85c7d3f0982ad77432e2443893c77b7dc8
Ananya Paul <manification10@users.noreply.github.com>
2019-12-04 16:48:26 -0500

    Update sound_bucket_2.ino

--- sound_bucket_2.ino
+++ sound_bucket_2.ino
@@ -7,7 +7,9 @@
 
 // I2C
 Adafruit_LIS3DH accelerometer = Adafruit_LIS3DH();
-#define CLICKTHRESHHOLD 80
+#define CLICKTHRESHHOLD1 50
+#define CLICKTHRESHHOLD2 100
+#define CLICKTHRESHHOLD3 150
 
 long steps = 0;                   // step count. reset every card write
 int minStepInterval = 50;         // min time between steps, in ms
@@ -30,37 +32,58 @@ void setup(void) {
   accelerometer.setRange(LIS3DH_RANGE_2_G);
   delay(100);
 
+  
+  // 1 = single click only interrupt output
+  accelerometer.setClick(1, CLICKTHRESHHOLD1);
+  accelerometer.setClick(2, CLICKTHRESHHOLD2);
+  accelerometer.setClick(2, CLICKTHRESHHOLD3);
+  delay(100);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  accelerometer.read();
-  x = accelerometer.x;
-  y = accelerometer.y;
-  z = accelerometer.z;
-
-// Uncomment and test with different /n 
-
-//  n = 500;
-//  x = x/n;
-//  y = y/n;
-//  z = z/n;
-  
-//  Serial.println("xyz values");
+//  accelerometer.read();
+//  x = accelerometer.x;
+//  y = accelerometer.y;
+//  z = accelerometer.z;
 //  
-//  Serial.println(x);
-//  Serial.println(y);
-//  Serial.println(z);
- 
-  if (( 30 < x && x < 160) && ( -1600 < y && y < 1800) && (1500 < z && z < 16000))
-  {Serial.println("0");}
-  else if (( -500 < x && x < 1000) && ( -2400 < y && y < 4000) && (4000 < z && z < 20000))
-  {Serial.println("1");}
-  else if (( -3000 < x && x < 2000) && ( -3000 < y && y < 6000) && (5000 < z && z < 25000))
-  {Serial.println("2");}
-  else
-  {Serial.println("3");}
- 
-  delay(500);
+////  Serial.println("xyz values");
+////  Serial.println(x);
+////  Serial.println(y);
+////  Serial.println(z);
+// 
+//  if (( -400 < x && x < 450) && ( -1600 < y && y < 2300) && (1500 < z && z < 17000))
+//  {Serial.println("0");}
+//  else if (( -500 < x && x < 1000) && ( -2400 < y && y < 4000) && (4000 < z && z < 20000))
+//  {Serial.println("1");}
+//  else if (( -3000 < x && x < 2000) && ( -3000 < y && y < 6000) && (5000 < z && z < 25000))
+//  {Serial.println("2");}
+//  else
+//  {Serial.println("3");}
+// 
+////  delay(300);
+  // read for clicks every 50 ms:
+  byte click = accelerometer.getClick();
+//  if (click > 0) {
+//    steps++;
+//    digitalWrite(9, HIGH);   // turn the LED on (HIGH is the voltage level)
+//    delay(500);                       // wait for a second
+//    digitalWrite(9, LOW);    // turn the LED off by making the voltage LOW
+//    delay(500);                       // wait for a second
+//    Serial.print("g");
+//    Serial.println(click);
+//  }
+    if (click > 10 ) {
+      if (click > 100) 
+      Serial.println("3");
+      else if (click > 30)
+      Serial.println("2");
+      else
+      Serial.println("1");
+      delay(200); 
+    }
+  //Serial.println(click);
+  
   
 }
